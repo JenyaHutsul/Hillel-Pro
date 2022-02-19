4a0ec35ae7f8b05df29c2a828f70f4c7b35b024c
Jenya Hutsul <jenyahutsul@gmail.com>
2022-02-19 23:02:01 +0300

    made hm10

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-function isObj(obj) {
-    return typeof obj === "object" && obj !== null && !Array.isArray(obj);
+/* доп задача */
+function getSum(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            sum += getSum(arr[i]);
+        }else{
+            sum+=arr[i];
+        }
+    }return sum;
 }
-/*Написать функцию “глубокого” копирования. Функция принимает один параметр и возвращает его копию “по значению”. В функцию можно передать параметр любого типа. 
-Если передали объект, то предусмотреть ситуацию, когда свойствами этого объекта будут объекты или массивы.
-Если передали массив, то предусмотреть ситуацию, когда элементами этого массива будут объекты или массивы.
-Если передали примитив - вернуть его. */
 
-function copy(origin) {
-    if(isObj(origin)){
-        let returnValue = {};
-        for(let key in origin){
-            if(isObj(origin[key])){
-                returnValue[key] = copy(origin[key])
-            }else{
-                returnValue[key] = origin[key]
-            }
-        }return returnValue
-    }else if(Array.isArray(origin)){
-        let returnValue = [];
-        for(let key in origin){
-            if(Array.isArray(origin[key])){
-                returnValue[key] = copy(origin[key])
-            }else{
-                returnValue[key] = origin[key]
-            }
-        }return returnValue;
-    }return origin;
-};
 
-const a = {
-    1:"DSfsdfsdfs",
-    2:"dsfsdgfsdfsdf",
-    "sdfsdfsdfsdfs": {1:2,45:"Fsfs"},
-    4:[1,2,3,4,5,[1,2,3,4,5],{"SDFgdfgdf":"fgffdsgdfgdf"}],
-    5: {1:[1,3,4,5,6,4,3,2]}
+/*Написать функцию generateList. Принимает массив из чисел и массивов чисел, например: [1, 2, [1.1, 1.2, 1.3], 3]. 
+Нужно сгенерировать список из элементов. Если в массиве встречается массив, то нужно сделать вложенный список. */
+
+function createLi(text){
+    const li = document.createElement("li");
+    li.innerText = text;
+    return li;
 }
-let b = copy(a)
-console.log(a,b);
+
+function createUl(){
+    return document.createElement("ul");
+}
+
+function createDom(arr){
+    const ul = createUl();
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            const li = createDom(arr[i]);
+            ul.append(li);
+        }else{
+            const li = createLi(arr[i]);
+            ul.append(li);
+        }
+    }return ul;
+}
