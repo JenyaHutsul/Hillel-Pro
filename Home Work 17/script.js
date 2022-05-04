7f3851c32f16a1a33b3eb20a6b5cfaaed894f979
Jenya Hutsul <jenyahutsul@gmail.com>
2022-05-04 18:19:39 +0300

    Remade renderClock

--- Home Work 17/script.js
+++ Home Work 17/script.js
@@ -11,12 +11,27 @@ function createTimeObj(date = new Date) {
     const m = date.getMinutes();
     const s = date.getSeconds();
 
-    return `${h} : ${m} : ${s}`;
+    return `${h}  ${m}  ${s}`;
 }
 
 
 function renderClock(dateString,el) {
-    el.textContent = dateString;
+    el.textContent = "";
+
+    const [ hour, col ,min, col2 ,sec ] = dateString.split(" ");
+
+    const arr = [[hour,"hour"],[col, "colon"],[min, "min"],[col2,"colon"],[sec, "sec"]].map(item => {
+        const el = createEl("div", item[1]);
+        el.textContent = item[0] === "" ? ":" : addZero(item[0]);
+
+        return el;
+    })
+
+    arr.forEach(item => el.appendChild(item));
+
+    function addZero(i){
+        return i > 10 ? i : "0" + i;
+    }
 } 
 
 function createEl(type, _class){
